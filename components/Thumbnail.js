184293d3f0138bd9a5b30cb7322e34ddb1d6470c
Aayush <karkiayush201@gmail.com>
2022-03-05 12:27:36 +0545

    fix bug

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,16 +1,13 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
-import { forwardRef } from "react";
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+function Thumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original";
   return (
-    <div
-      ref={ref}
-      className=" p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 "
-    >
+    <div className=" p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 ">
       <Image
         layout="responsive"
+        alt={`${result.title || result.original_name}`}
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
           `${BASE_URL}${result.poster_path}`
@@ -32,6 +29,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       </div>
     </div>
   );
-});
+}
 
 export default Thumbnail;
